b7f7c7ac3456cdb38a70a8f0c06e4df7a034550a
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-06-02 23:28:00 +0200

    deleted default versionKey from model properties (__v)

--- backend/models/Activity.js
+++ backend/models/Activity.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose")
 const schema = mongoose.Schema({
 	name: String,
 	properties: String,
+},{
+	versionKey: false 
 })
 
 module.exports = mongoose.model("Activity", schema)

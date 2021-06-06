7c72848c87872027fdea2384c6e25070fae29850
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-06-06 23:40:06 +0200

    Not working apicaller

---
+++ app/core/ApiCaller.ts
@@ -0,0 +1,39 @@
+import Settings from './Settings';
+
+class ApiCaller {
+    apiUrl: string;
+    constructor( apiUrl?: string) {
+        this.apiUrl = apiUrl ? apiUrl : Settings.getApiURL() 
+    }
+
+    async call(url:string, method:string, params?:object) : Promise<any>{ 
+        var fetchParams:any = {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        if(params) {
+            fetchParams.body = JSON.stringify(params); // convert params to string
+        }
+        // define separator to fetch params to the URL.
+        let separator = '?';
+        if(url.indexOf('?') != -1) {
+            separator = '&';
+        }
+
+        var ApiRoute = url;
+        //Settings.getApiURL()+ApiRoute
+        return fetch(Settings.getApiURL()+ApiRoute, fetchParams)
+            .then(function(response) {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                return response;
+            })
+            .then(response => response.json())
+    }
+}
+
+export default ApiCaller;

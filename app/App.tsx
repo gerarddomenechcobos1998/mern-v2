9bd94d19b40ee75d64d0cae8878d0e62d7b58882
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-06-09 00:30:16 +0200

    Added button to call GET activity by id

--- app/App.tsx
+++ app/App.tsx
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import ApiCaller from './api/ApiCaller';
 
 export default function App() {
   // llamada a la api
   var apiCaller = new ApiCaller();
-  let tmp:any = "No encontrado"; 
-  useEffect(()=>{
-    tmp = apiCaller.call('/activities', 'GET');
-  },[])
+  const [data,setData] = React.useState([]);
+  let tmp = undefined;
+  // get info about 1 activity
+  const buttonPress = async ()=>{
+    tmp = await apiCaller.call('/activities/60b7f7740594d00f557a5140', 'GET');
+    console.log('Response', tmp);
+    setData(JSON.stringify(tmp))
+  }
   
   // fin llamada a la api
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>{tmp}</Text>
+      <Button onPress={buttonPress}>Obtain data</Button>
+      <Text>{data}</Text>
       <StatusBar style="auto" />
     </View>
   );

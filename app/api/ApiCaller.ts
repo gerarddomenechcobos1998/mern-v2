fee02afe2dc73af6b8e64f9bfad01f5e7552bc2b
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-06-09 00:30:52 +0200

    Added api caller

--- app/api/ApiCaller.ts
+++ app/api/ApiCaller.ts
@@ -1,9 +1,12 @@
+import Settings from '../core/Settings';
+
 class ApiCaller {
-    constructor() {
+    apiUrl: string;
+    constructor( apiUrl?: string) {
+        this.apiUrl = apiUrl ? apiUrl : Settings.getApiURL() 
     }
 
-    async call(url:string, method:string, params?:object) : Promise<any>{
-        
+    async call(url:string, method:string, params?:object) : Promise<any>{ 
         var fetchParams:any = {
             method: method,
             headers: {
@@ -12,9 +15,17 @@ class ApiCaller {
         }
 
         if(params) {
-            fetchParams.body = JSON.stringify(params);
+            fetchParams.body = JSON.stringify(params); // convert params to string
         }
-        return fetch('http://localhost:3000/api', fetchParams)
+        // define separator to fetch params to the URL.
+        let separator = '?';
+        if(url.indexOf('?') != -1) {
+            separator = '&';
+        }
+
+        var ApiRoute = url;
+        //Settings.getApiURL()+ApiRoute
+        return fetch(Settings.getApiURL()+ApiRoute, fetchParams)
             .then(function(response) {
                 if (!response.ok) {
                     throw Error(response.statusText);

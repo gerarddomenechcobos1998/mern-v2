ae9cc4eb1e39500472d947bcf0a38b3a19bf1a00
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-06-10 22:49:47 +0200

    Added cors policy to allow ALL (care or change the rule to don't have security problems)

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 // FONT: https://rahmanfadhil.com/express-rest-api/
 const PORT = 3000;
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
@@ -10,7 +11,10 @@ mongoose
 	.then(() => {
 		const app = express();
 		app.use(express.json()); /*Express by default is not able to read a request body if we no use this*/
+		// CORS policy
+		app.use(cors());
 		app.use("/api", routes); /* The "/api" is the prefix, so we can change it to other prefix */
+		// Start server at port specified
 		app.listen(PORT, () => {
             console.log("Server has started! ", PORT);
         })

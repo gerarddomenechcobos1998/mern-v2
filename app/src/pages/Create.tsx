848840b9c11ad0649dbae576fd9fb81a3f7b6cfb
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-06-19 15:09:13 +0200

    Create Screen Added

--- app/src/pages/Create.tsx
+++ app/src/pages/Create.tsx
@@ -1,34 +1,54 @@
 import React, { memo, useState } from 'react';
-import { View, Text,Alert,StyleSheet} from 'react-native';
-
+import { View, Text} from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
 import { Navigation } from '../types';
 import { theme } from '../core/theme';
+import ApiCaller from '../core/ApiCaller';
 
 type Props = {
   navigation: Navigation;
 };
 
 const CreateScreen = ({ navigation }: Props) => {
+  const [name, setName] = useState<string>('');
+  const [properties, setProperties] = useState<string>('');
 
-return (
-        <View style={{ flex:1}}>
-                <Text>Create Screen</Text>
-        </View>
-    );
+  var apiCaller = new ApiCaller();
+
+  const createActivity = async (activity:any) => {
+    const activityRes = await apiCaller.call('/activities', 'POST', activity);
+    navigation.navigate('view', {id: activityRes?._id})
+  }
+  const onCreatePress = () => {
+    let activityTmp = {};
+    activityTmp.name = name;
+    activityTmp.properties = properties;
+    createActivity(activityTmp);
+    setName('');
+    setProperties('');
+  }
+  return (
+    <View style={{ flex:1}}>
+      <View style={{alignSelf:'center', width:'40%', marginTop:50}}>
+        <TextInput
+            mode= 'outlined'
+            label='Nombre'
+            placeholder='Nombre de la actividad'
+            value={name}
+            onChangeText={text=> setName(text)}
+            style={{marginBottom:20}}
+        />
+          <TextInput
+            mode= 'outlined'
+            label='Propiedades'
+            placeholder='Propiedades de la actividad'
+            value={properties}
+            onChangeText={text=> setProperties(text)}
+            style={{marginBottom:60}}
+        />
+        <Button mode='contained' style={{ alignSelf:'center', width:200}} uppercase={false} onPress={()=> onCreatePress()}>Crear</Button>
+      </View>
+    </View>
+  );
 };
-
-
-const styles = StyleSheet.create({
-  entry: {
-    backgroundColor: theme.colors.background,
-
-  },
-  label: {
-    color: theme.colors.text,
-  },
-  link: {
-    fontWeight: 'bold',
-    color: theme.colors.primary,
-  },
-});
 export default memo(CreateScreen);

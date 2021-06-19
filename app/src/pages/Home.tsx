1feca921d24ce8c1141ff3dd87bdec32c97575fa
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-06-19 15:09:45 +0200

    Home Screen Added with list of all activities

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import React, { memo, useState, useEffect } from 'react';
-import { View, Text,Alert,StyleSheet, ScrollView} from 'react-native';
-import { Card, Title, Paragraph  } from 'react-native-paper';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import { Card, Title, Paragraph, Portal, Dialog, Button, FAB } from 'react-native-paper';
 import { Navigation } from '../types';
 import { theme } from '../core/theme';
 import ApiCaller from '../core/ApiCaller';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 type Props = {
   navigation: Navigation;
@@ -11,39 +12,91 @@ type Props = {
 
 const HomeScreen = ({ navigation }: Props) => {
   const [activites,setActivities] = useState<any>();
+  const [deleteId, setDeleteId] = useState<string>();
+  const [dialogVisible, setDialogVisible] = useState<boolean>(false);
   var apiCaller = new ApiCaller();
 
   const readArticles = async ()=>{
     const dataRes = await apiCaller.call('/activities', 'GET');
     setActivities(dataRes); 
   }
+  const deleteActivity = async ()=>{
+    await apiCaller.call('/activities/'+deleteId, 'DELETE');
+  }
+  const onDeleteActivity = (id:string)=>{
+    setDeleteId(id);
+    setDialogVisible(true);
+  }
+
+  const confirmDelete = () => {
+    deleteActivity();
+    setDialogVisible(false); 
+  }
+
   useEffect(()=>{
     readArticles();
-  },[])
+  },[dialogVisible])
 
-return (
-        <View style={{ flex:1, flexDirection:'column', justifyContent:'center', alignContent:'center'}}>
-          <View style={{ width:'60%', flex:1, flexDirection:'column', justifyContent:'center', alignSelf:'center', borderWidth:1}}>
-          <ScrollView
-            style={{ height:0}}
-            contentContainerStyle={{flexDirection:'row', flexWrap:'wrap', justifyContent:'center'}}
-          >
-            {
-              activites?activites.map((activity:any, index:number) =>{
-                return(
-                  <Card key={index} style={{width:300, margin:20}} onPress={()=>navigation.navigate('view', {id: activity?._id})}>
-                    <Card.Content>
+  return (
+    <View style={{ flex:1, flexDirection:'column', justifyContent:'center', alignContent:'center'}}>
+      <View style={{ width:'60%', flex:1, flexDirection:'column', justifyContent:'center', alignSelf:'center'}}>
+      <ScrollView
+        style={{ height:0}}
+        contentContainerStyle={{flexDirection:'row', flexWrap:'wrap', justifyContent:'center'}}
+      >
+        {
+          activites?activites.map((activity:any, index:number) =>{
+            return(
+              <Card key={index} style={{width:300, margin:20}} onPress={()=>navigation.navigate('view', {id: activity?._id})}>
+                <Card.Content>
+                  <View style={{flexDirection:'row'}}>
+                    <View style={{flexDirection:'column', flex:2}}>
                       <Title>{activity?.name}</Title>
                       <Paragraph>{activity?.properties}</Paragraph>
-                    </Card.Content>
-                  </Card>
-                );
-              }):null
-            }
-          </ScrollView>
-          </View>
-        </View>
-    );
+                    </View>
+                    <View style={{flexDirection:'column', justifyContent:'center', alignItems:'flex-end', flex:1}}>
+                      <MaterialCommunityIcons
+                        name="pencil"
+                        color='#4566f7'
+                        size={20}
+                        style={{marginBottom:10}}
+                        onPress={()=>navigation.navigate('update', {id: activity?._id})}
+                      />
+                      <MaterialCommunityIcons
+                        name="delete"
+                        color='#d12424'
+                        size={20}
+                        onPress={()=> onDeleteActivity(activity?._id)}
+                      />
+                    </View>
+                  </View>
+                </Card.Content>
+              </Card>
+            );
+          }):null
+        }
+      </ScrollView>
+      <View style={{flexDirection:'column', alignSelf:'center', marginVertical:20}}>
+        <FAB
+          style={{}}
+          small
+          icon="plus"
+          onPress={() => navigation.navigate('create')}
+        />
+      </View>
+      </View>
+      {/*Delete dialog*/}
+      <Portal>
+        <Dialog style={{width:400, alignSelf:'center'}} visible={dialogVisible} onDismiss={()=> setDialogVisible(false)}>
+          <Dialog.Title style={{textAlign:'center'}}>Desea eliminar la actividad?</Dialog.Title>
+          <Dialog.Actions style={{alignSelf:'center'}}>
+            <Button mode='outlined' uppercase={false} onPress={()=> setDialogVisible(false)}>Cancelar</Button>
+            <Button labelStyle={{color:'white'}} style={{backgroundColor:'#d12424', marginLeft:20}} uppercase={false}onPress={confirmDelete}>Eliminar</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+    </View>
+  );
 };
 
 

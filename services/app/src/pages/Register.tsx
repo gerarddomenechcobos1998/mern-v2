9fab78e8d0bb8b45070bc1ed0e8d37f9138e6dff
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-06-24 12:36:48 +0200

    Generated register with encrypted (using Hash) password

--- services/app/src/pages/Register.tsx
+++ services/app/src/pages/Register.tsx
@@ -14,11 +14,14 @@ const RegisterScreen = ({ navigation }: Props) => {
     const [ email, setEmail ] = useState<string>('');
     const [ password, setPassword ] = useState<string>('');
     const [ rePassword, setRePassword ] = useState<string>('');
+    const [ visible, setVisible ] = useState<boolean>(false);
+    const [ hash, setHash ] = useState('');
+
+    var apiCaller = new ApiCaller();
     
     const emailValidator = (email: string) =>{
         console.log(email);
-        // estructura de un email:  nombre_usuario+@+servidor+dominio, eg pitu@gmail.com
-        // SOURCE: http://lineadecodigo.com/javascript/validar-el-email-con-javascript/
+        // estructura de un email:  nombre_usuario+@+servidor+dominio, eg: user6840@gmail.com
         // use of Regex to validate
         const regexValidator = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         const result = regexValidator.test(email)? true:false;
@@ -30,10 +33,38 @@ const RegisterScreen = ({ navigation }: Props) => {
         return result;
     }
 
-    const onRegisterPress = () => {
+    const onToggleShowPassword = () => {
+        setVisible(!visible);
+    }
+
+    const generateHash = (password:string) => {
+        const bcrypt = require("bcryptjs")
+        const saltRounds = 10;
+        bcrypt.genSalt(saltRounds, function (saltError: any, salt: any) {
+            if (saltError) {
+              throw saltError
+            } else {
+              bcrypt.hash(password, salt, function(hashError:any, hash:any) {
+                if (hashError) {
+                  throw hashError
+                } else {
+                  setHash(hash);
+                }
+              })
+            }
+        })
+    }
+
+    const onRegisterPress = async () => {
         // if (emailValidator(email) && validarContraseña(password, rePassword)){
         if (validarContraseña(password, rePassword) && emailValidator(email)){
-            console.log("registrado");
+            // generate hash to encrypt the password
+            generateHash(password);
+            let profile: any = {};
+            profile.email = email;
+            profile.password = hash;
+            console.log(hash);
+            await apiCaller.call('/profile','POST', profile);
         }
         else{
             console.log("no registrado");
@@ -43,33 +74,41 @@ const RegisterScreen = ({ navigation }: Props) => {
     return (
         <View style={{ flex:1 }}>
         <View style={{alignSelf:'center', flex:1, width:'40%', marginBottom:'5%', marginTop:50}}>
-            <View style={{ flex:10, flexDirection:'column', borderWidth:1 }}>
+            <View style={{ flex:10, flexDirection:'column'}}>
             <TextInput
                 mode= 'outlined'
                 label='Email'
                 placeholder='Email'
                 value={email}
+                autoCompleteType='email'
+                keyboardType='email-address'
                 onChangeText={text=> setEmail(text)}
-                style={{marginBottom:40}}
+                style={{marginBottom:40}}            
             />
             <TextInput
                 mode= 'outlined'
                 label='Contraseña'
                 placeholder='Contraseña'
                 value={password}
+                autoCompleteType='password'
+                autoCorrect= {false}
                 onChangeText={text=> setPassword(text)}
                 style={{marginBottom:40}}
+                secureTextEntry={!visible}
+                right= {<TextInput.Icon name={visible?'eye-off':'eye'} onPress={()=> onToggleShowPassword()}/>}
             />
             <TextInput
                 mode= 'outlined'
                 label='Repetir contraseña'
                 placeholder='Repetir contraseña'
                 value={rePassword}
+                autoCompleteType='password'
+                autoCorrect= {false}
                 onChangeText={text=> setRePassword(text)}
-                style={{}}
+                secureTextEntry={!visible}
             />
             </View>
-            <View style={{ flex:1, flexDirection:'column', justifyContent:'flex-start', borderWidth: 1 }}>
+            <View style={{ flex:1, flexDirection:'column', justifyContent:'flex-start' }}>
                 <Button mode='contained' style={{ alignSelf:'center', width:200}} uppercase={false} onPress={()=> onRegisterPress()}>Registrarse</Button>
             </View>
         </View>

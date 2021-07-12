b88c6d48dfbe9aaf1c14eb4e47c0ed6eee91c8c7
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-07-12 23:33:47 +0200

    Create an UserReducer that allow to set User with context provider

---
+++ services/app/src/context/user/userReducer.ts
@@ -0,0 +1,13 @@
+
+export default function userReducer(state:any, action:any) {
+    switch (action.type) {
+      case "SET_USER":
+        return {
+          ...state,
+          user: action.payload
+        };
+
+      default:
+        return state;
+    }
+  };

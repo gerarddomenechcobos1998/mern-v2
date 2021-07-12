27d46d79423720dad19679a0d4a77025a0c21412
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-07-12 23:34:37 +0200

    Created functions to get user  from localstorage at Settings

--- services/app/src/core/Settings.ts
+++ services/app/src/core/Settings.ts
@@ -1,9 +1,21 @@
 import apiUrl from './environment';
+import User from '../models/user';
+import Storage from './Storage';
 
+const getCurrentUser = async (): Promise<User> => {
+    const data = await Storage.read('user');
+    const currentUser = User.prototype.load(data);
+    return currentUser;
+}
+const logout = async () =>{
+    await Storage.remove('user')
+}
 var Settings = {
     getApiURL: () => {
         return apiUrl;
-    }
+    },
+    getCurrentUser: getCurrentUser,
+    logout: logout
 }
 
 export default Settings;

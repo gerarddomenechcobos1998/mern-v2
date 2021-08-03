2ce8ca13d392cdec2523f76d07ac7a425d17e15b
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-08-03 23:32:34 +0200

    Added new menu for !web, draggable bottom sheet

--- services/app/src/components/Header.tsx
+++ services/app/src/components/Header.tsx
@@ -1,7 +1,8 @@
-import React, { memo } from 'react';
+import React, { memo, useRef } from 'react';
 import { Appbar } from 'react-native-paper';
 import {theme} from '../core/theme';
-
+import {Platform, View} from 'react-native';
+import RBSheet from "react-native-raw-bottom-sheet";
 
 type Props = {
     previous: any;
@@ -10,31 +11,59 @@ type Props = {
 };
  
 const Header = ({ previous, navigation, headerBorder}: Props) => {
+    const refRBSheet = useRef();
+
     const menuButton = () => {
         return <Appbar.Action icon="menu" onPress={() => {
-            navigation.openDrawer();
+            Platform.OS== 'web'?navigation.openDrawer(): refRBSheet.current.open();
         }} />
     }
    
     return (
-    <Appbar.Header style={{backgroundColor:theme.colors.navBar, height: 'auto', borderBottomColor: '#e3e3e3', elevation:0, borderBottomWidth: headerBorder }}>
+    <>  
+        <Appbar.Header style={{backgroundColor:theme.colors.navBar, height: 'auto', borderBottomColor: '#e3e3e3', elevation:0, borderBottomWidth: headerBorder }}>
+            {
+                previous 
+                ?<Appbar.BackAction onPress={navigation.goBack}/>
+                : null
+            }
+            
+            <Appbar.Content
+                    title={'Hello from app'}
+                    titleStyle={{
+                        fontSize: 18,
+                        fontWeight: 'bold',
+                        color: theme.colors.primary
+                    }}
+                >
+            </Appbar.Content>
+            { menuButton()}
+        </Appbar.Header>
         {
-            previous 
-            ?<Appbar.BackAction onPress={navigation.goBack}/>
-            : null
+        Platform.OS !='web'?
+        <RBSheet
+            ref={refRBSheet}
+            closeOnDragDown={true}
+            closeOnPressMask={false}
+            openDuration={250}
+            height={400}
+            closeOnPressMask={true}
+            customStyles={{
+                wrapper: {
+                    backgroundColor: "rgba(0,0,0,0.1)"
+                },
+                container:{
+                    borderTopLeftRadius:20,
+                    borderTopRightRadius:20,
+                },
+                draggableIcon: {
+                    backgroundColor: "rgba(0,0,0,0.3)"
+                }
+                
+            }}
+        />:null
         }
-        
-        <Appbar.Content
-                title={'Hello from app'}
-                titleStyle={{
-                    fontSize: 18,
-                    fontWeight: 'bold',
-                    color: theme.colors.primary
-                }}
-            >
-        </Appbar.Content>
-        { menuButton()}
-    </Appbar.Header>);
+    </>);
 };
 
 export default memo(Header);

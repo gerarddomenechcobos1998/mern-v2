5e70ec1f859fdaa4c95888d6537e4d21310ba9c4
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-08-03 10:16:31 +0200

    Added custom drawer content (used as Menu)

--- services/app/src/components/DrawerContent.tsx
+++ services/app/src/components/DrawerContent.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import {
-  DrawerContentScrollView,
-} from '@react-navigation/drawer';
-import {
-  Divider,
-} from 'react-native-paper';
+import { DrawerContentScrollView } from '@react-navigation/drawer';
 
 import Menu from './Menu';
 
@@ -13,8 +8,7 @@ export function DrawerContent(props:any) {
 
   return (
     <DrawerContentScrollView contentContainerStyle={styles.drawerContent} {...props}>
-      <Divider style={{ position: 'absolute', opacity: 0.8, left: 0, width: 1, height: '100%' }} />
-      <Menu></Menu>
+      <Menu navigation={props.navigation}/>
     </DrawerContentScrollView>
   );
 }

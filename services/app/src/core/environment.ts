4bf18c9e635635f9436bc3c2f18d7d45745c4036
gerard.domenech.cobos <gerard.domenech.cobos@gmail.com>
2021-08-27 20:02:08 +0200

    now environments has only the name of the server, but not the port to redirect api calls

--- services/app/src/core/environment.ts
+++ services/app/src/core/environment.ts
@@ -1,5 +1,5 @@
 // IP of the server that is hosting the Data Base. Port of the express api, you can check the extension at index.js (at backend folder)
-const localhost = 'http://localhost:1880';
+const localhost = 'http://localhost';
 // Extension of the api: 'api', you can check the extension at index.js (at backend folder)
 const apiExtension = '/api';
 // The apiURL is the composition of two strings
